cb8203d8330215dce199d7b9beb0afd26c0440fd
Emanuel Rodriguez Anitori <88413740+Anitori@users.noreply.github.com>
2022-08-14 16:34:55 -0300

    Add 1

--- 4-Mostrar y Aceptar datos en GO/main.go
+++ 4-Mostrar y Aceptar datos en GO/main.go
@@ -1,1 +1,20 @@
 package main
+
+//Puede llegar a tener utilidad en backoffice
+
+import (
+	"fmt"
+	"os"
+	"bufio" //Alternativa de aceptar cosas por teclado
+)
+
+var numero1 int
+var numero2 int
+
+func main() {
+	fmt.Println("Ingrese número 1: ")
+	fmt.Scanln(&numero1) // &puntero
+
+	fmt.Println("Ingrese número 2: ")
+	fmt.Scanln(&numero2)
+}
